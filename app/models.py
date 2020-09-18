cbbef1e4a9034e6de9c04f02b942459a13e64f68
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-09-18 22:20:18 +0300

    pitches class added

--- app/models.py
+++ app/models.py
@@ -1,4 +1,24 @@
 from . import db
+from datetime import datetime
+
+class Pitches(db.Model):
+
+    __tablename__ = 'pitches'
+    
+    id = db.Column(db.Integer,primary_key = True)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    pitch = db.Column(db.String)
+    
+    def save_pitch(self):
+      db.session.add(self)
+      db.session.commit()
+      
+    @classmethod
+    def get_pitches(cls):
+      pitches=Pitch.query.filter_by()
+    
+
 
 
 
