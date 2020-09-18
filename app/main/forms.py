7ca9434d1249f9a870c4b8177214808836b8809b
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-09-19 01:22:55 +0300

    user model initiated

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,5 +5,8 @@ from wtforms.validators import Required
 class PitchForm(FlaskForm):
     category = StringField('Pitch category')
     title = StringField('Pitch title',validators=[Required()])
-    pitch = TextAreaField('Pitch', validators=[Required()])
+    pitch = TextAreaField('Pitch', )
     submit = SubmitField('Submit')
+    
+    
+class Useer(db.Model):    
